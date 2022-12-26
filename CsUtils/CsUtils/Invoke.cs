523c76eecc774168a344e8a019a2326c2cc330da
ke152 <ke152@foxmail.com>
2022-12-26 22:15:20 +0800

    init AesEncrypt && init Invoke.Delay

---
+++ CsUtils/CsUtils/Invoke.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CsUtils
+{
+    public class Invoke
+    {
+        public static void Delay<T1, T2>(int delay, Delegate func, T1 t1, T2 t2)
+        {
+            Task.Run(async () =>
+            {
+                await Task.Delay(delay);
+                dynamic d = func;
+                d.Invoke(t1, t2);
+            });
+        }
+    }
+}
---
+++ CsUtils/Example/Program.cs
@@ -0,0 +1,7 @@
+﻿using CsUtils;
+
+Invoke.Delay(1000, Add, 3, 5);
+
+void Add(int a, int b) => Console.WriteLine($"{a}+{b}={a + b}");
+
+await Task.Delay(3000);
