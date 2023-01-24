e28e9df97ab6c54601047fcc9e336c9d8998908a
ke152 <ke152@foxmail.com>
2023-01-24 13:00:12 +0800

    添加单例

---
+++ CsUtils/Example/Bag.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CsUtils;
+
+namespace Example
+{
+    internal class Item
+    {
+        public int Id = 200;
+        public string Name = "物品";
+    }
+
+    internal class Bag
+    {
+        public int Id = 100;
+        public string Name = "背包";
+    }
+
+    internal class BagManager : Singleton<BagManager>
+    {
+        public int Id = 100;
+        public string Name = "背包管理器";
+    }
+}
--- CsUtils/Example/Program.cs
+++ CsUtils/Example/Program.cs
@@ -1,7 +1,3 @@
 ﻿using CsUtils;
 
-Invoke.Delay(1000, Add, 3, 5);
-
-void Add(int a, int b) => Console.WriteLine($"{a}+{b}={a + b}");
-
-await Task.Delay(3000);
+Singleton
