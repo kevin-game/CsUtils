6eb0c0acb4dba475de806f703f7f2af076235a98
ke152 <ke152@foxmail.com>
2023-02-26 00:32:17 +0800

    add TypeScanner

---
+++ CsUtils/TypeScanner.cs
@@ -0,0 +1,58 @@
+using System.Reflection;
+
+namespace CsUtils;
+
+public static class TypeScanner
+{
+    static readonly Dictionary<Type, List<Type>> _attrToTypes = new();
+    static readonly Dictionary<Type, List<Type>> _interfaceToTypes = new();
+
+    static TypeScanner()
+    {
+        var types = AppDomain.CurrentDomain.GetAssemblies().SelectMany(a => a.GetExportedTypes())
+            .Where(t => !IsSystemType(t));
+
+        foreach (var type in types)
+        {
+            // attributes to types
+            var attrs = type.GetCustomAttributes().Where(a => !IsSystemType(a.GetType()));
+            foreach (var attribute in attrs)
+            {
+                DictionarySafeAdd(_attrToTypes, attribute.GetType(), type);
+            }
+            
+            // interfaces to types
+            var interfaces = type.GetInterfaces().Where(i => !IsSystemType(i.GetType()));
+            foreach (var i in interfaces)
+            {
+                DictionarySafeAdd(_interfaceToTypes, i, type);
+            }
+            
+            
+        }
+    }
+
+    static bool IsSystemType(Type type)
+    {
+        if (type.FullName == null) return false;
+        if (type.FullName.StartsWith("Microsoft.Win32")) return true;
+        if (type.FullName.StartsWith("Internal.")) return true;
+        if (type.FullName.StartsWith("System.")) return true;
+        return false;
+    }
+
+    static void DictionarySafeAdd<TKey, TValue>(Dictionary<TKey, List<TValue>> dict, TKey key, TValue value)
+    {
+        if (!dict.ContainsKey(key)) 
+            dict[key] = new List<TValue>();
+        dict[key].Add(value);
+    }
+    
+    public static List<Type>? GetTypesWithAttribute<TAttribute>()
+    {
+        if (!_attrToTypes.ContainsKey(typeof(TAttribute)))
+            return _attrToTypes[typeof(TAttribute)].ToList();
+
+        return null;
+    }
+}
--- Example/Program.cs
+++ Example/Program.cs
@@ -1,3 +1,13 @@
-﻿using CsUtils;
+﻿using System.Security.Cryptography;
+using System.Threading.Channels;
+using CsUtils;
 using CsUtils.Algorithm;
 
+var types = TypeScanner.GetTypesWithAttribute<FlagsAttribute>();
+
+var a = 3;
+var b = 5;
+Invoke.Delay(5000, ()=>b = b+a);
+Console.WriteLine($"b={b}");
+Thread.Sleep(6000);
+Console.WriteLine($"b={b}");
