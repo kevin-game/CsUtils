20afb67a6339f89fd68028f2d5bb09726434dfaa
ke152 <ke152@foxmail.com>
2023-08-14 00:07:53 +0800

    add：PluginManager基于Assembly的简单插件模式

---
+++ src/CsUtils/PluginManager.cs
@@ -0,0 +1,17 @@
+﻿using Plugin.Common;
+using System.Reflection;
+
+var dllList = Directory.EnumerateFiles(Directory.GetCurrentDirectory(), "*.dll");
+
+foreach (var dll in dllList)
+{
+    var assembly = Assembly.LoadFile(dll);
+    var types = assembly.GetExportedTypes();
+    var pluginTypes = types.Where(type => type.IsAssignableTo(typeof(IPlugin)));
+    foreach (var pluginType in pluginTypes)
+    {
+        var plugin = Activator.CreateInstance(pluginType) as IPlugin;
+        plugin?.Run();
+    }
+}
+
